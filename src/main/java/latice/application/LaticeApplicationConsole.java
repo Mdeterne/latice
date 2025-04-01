5531119fd82662b96d50f67fa3d0e492281f14d2
julian <rayconstantyjulian@gmail.com>
2025-04-01 15:19:27 +0200

    Ajout d'une nouvelle instruction dans main

--- src/main/java/latice/application/LaticeApplicationConsole.java
+++ src/main/java/latice/application/LaticeApplicationConsole.java
@@ -3,8 +3,9 @@ package latice.application;
 public class LaticeApplicationConsole {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+            System.out.println("-----------------------------------------------------");
+            System.out.println("-- Bienvenue dans notre magnifique jeu de latice ! --");
+            System.out.println("-- développé par Julian                            --");
+            System.out.println("-----------------------------------------------------");
+    }
 }
