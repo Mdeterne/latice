a5786526368849f9f4efe7ab0cab481e6036601c
julian <rayconstantyjulian@gmail.com>
2025-04-09 15:44:22 +0200

    ajout class Case

---
+++ src/main/java/latice/model/Case.java
@@ -0,0 +1,12 @@
+package latice.model;
+
+
+public class Case {
+	private boolean bonus;
+	private Position position;
+	
+	public Case(boolean bonus, Position position) {
+		this.bonus = bonus;
+		this.position = position;
+	}
+}
