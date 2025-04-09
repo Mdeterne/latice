ce3018a3c352534e66bd9926bf7198b6fe6362df
Mateo Deterne <mateodeterne@gmail.com>
2025-04-09 15:34:02 +0200

    ajout classe Pioche

---
+++ src/main/java/latice/model/Pioche.java
@@ -0,0 +1,14 @@
+package latice.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pioche {
+
+	private ArrayList piochePrincipal = new ArrayList<Jeton>();
+	private ArrayList piochePersonel = new ArrayList<Jeton>();
+	
+	public Pioche(int nbJeton) {
+		//TODO 
+	}
+}
