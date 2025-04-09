75d4684d77b1c276f72a63c40b5cf18770666441
julian <rayconstantyjulian@gmail.com>
2025-04-09 16:07:17 +0200

    Ajout méthodes dans plateau.

--- src/main/java/latice/model/Plateau.java
+++ src/main/java/latice/model/Plateau.java
@@ -8,15 +8,15 @@ public class Plateau {
 		plateau = new Case[9][9];
 	}
 
-	public donnerPoint(Joueur joueur, int nombres_points) {
+	public void donnerPoint(Joueur joueur, int nombres_points) {
 		//TODO Implémenter la méthode donner_point
 	}
 	
-	public poserJeton(Position position) {
+	public void poserJeton(Position position) {
 		//TODO Implémenter la méthode poserJeton
 	}
 	
-	public calculerPoint() {
+	public void calculerPoint() {
 		//TODO Implémenter la méthode calculerPoint
 	}
 }
