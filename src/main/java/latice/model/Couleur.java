68a50b49c1b853992e6be44e9b0090511da6feff
Benjamin Marty <martybenjamin75@gmail.com>
2025-04-09 14:57:22 +0200

    Ajout des énumérations.

---
+++ src/main/java/latice/model/Couleur.java
@@ -0,0 +1,11 @@
+package latice.model;
+
+public enum Couleur {
+	ROUGE,
+	BLEU,
+	BLEU_CLAIRE,
+	VERT,
+	JAUNE,
+	VIOLET
+
+}
---
+++ src/main/java/latice/model/Symbole.java
@@ -0,0 +1,11 @@
+package latice.model;
+
+public enum Symbole {
+	FLEUR,
+	LEZARD,
+	PLUME,
+	OISEAU,
+	DAUPHIN,
+	TORTUE
+
+}
