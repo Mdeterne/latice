b65213ab730aa78f50d1186e9264f4e5fd40638e
julian <rayconstantyjulian@gmail.com>
2025-04-09 15:44:48 +0200

    Ajout de la class plateau.

---
+++ src/main/java/latice/model/Plateau.java
@@ -0,0 +1,22 @@
+package latice.model;
+
+
+public class Plateau {
+	private Case plateau[][];
+	
+	public Plateau() {
+		plateau = new Case[9][9];
+	}
+
+	public donnerPoint(Joueur joueur, int nombres_points) {
+		//TODO Implémenter la méthode donner_point
+	}
+	
+	public poserJeton(Position position) {
+		//TODO Implémenter la méthode poserJeton
+	}
+	
+	public calculerPoint() {
+		//TODO Implémenter la méthode calculerPoint
+	}
+}
