dc488209bebc5631c2fe4768efe894e59bebdf94
Benjamin Marty <martybenjamin75@gmail.com>
2025-05-09 14:57:18 +0200

    ajout methode start avec exception dans LaticeJavaFXApplication

--- src/main/java/latice/application/JavaFX/LaticeJavaFXApplication.java
+++ src/main/java/latice/application/JavaFX/LaticeJavaFXApplication.java
@@ -1,5 +1,27 @@
 package latice.application.JavaFX;
 
-public class LaticeJavaFXApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class LaticeJavaFXApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Scene.fxml"));
+            
+            Scene scene = new Scene(root);
+
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Latice Game");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
