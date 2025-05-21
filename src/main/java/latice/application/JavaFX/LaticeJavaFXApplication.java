b0fe5678611377486d29c31c310c5d50cfbf0b9a
Mateo Deterne <mateodeterne@gmail.com>
2025-05-21 18:46:48 +0200

    adaptation nouveau fxml

--- src/main/java/latice/application/JavaFX/LaticeJavaFXApplication.java
+++ src/main/java/latice/application/JavaFX/LaticeJavaFXApplication.java
@@ -1,11 +1,9 @@
 package latice.application.JavaFX;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class LaticeJavaFXApplication extends Application {
