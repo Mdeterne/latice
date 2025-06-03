ecdb872181ff6fee2d856b8227e7c058693977ac
Julian Barre <barrejulian05@gmail.com>
2025-06-03 13:42:35 +0200

    Ajout Logo

--- src/main/java/latice/application/JavaFX/LaticeJavaFXApplication.java
+++ src/main/java/latice/application/JavaFX/LaticeJavaFXApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class LaticeJavaFXApplication extends Application {
@@ -13,8 +14,7 @@ public class LaticeJavaFXApplication extends Application {
         	
 			Parent root = FXMLLoader.load(getClass().getResource("/Scene1.fxml"));
 			Scene scene = new Scene(root);
-			
-			
+            primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/img/LogoLatice.jpg")));
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Latice Game");
 			primaryStage.show();
